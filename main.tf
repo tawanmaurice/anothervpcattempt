b8ec259aee5cc86561a78c92d5419ba17ec82313
tawanmaurice <214193274+tawanmaurice@users.noreply.github.com>
2025-10-18 13:38:10 -0400

    feat: add Terraform VPC code

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ resource "aws_vpc" "app1" {
   cidr_block = "10.32.0.0/16"
 
   tags = {
-    Name = "app1"
+    Name    = "app1"
     Service = "application1"
-    Owner = "Chewbacca"
-    Planet = "Mustafar"
+    Owner   = "Chewbacca"
+    Planet  = "Mustafar"
   }
 }
